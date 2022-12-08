934ce09e93b4034866879d408388e69c39164a23
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-12-07 18:37:36 -0600

    Create MathChallenge WebApp

---
+++ Basic_C#_Programs/MathChallenge/MathChallenge/MathChallenge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 7;
+            int total = num1 + num2;
+            Console.WriteLine(num1 + " plus " + num2 + " equals " + total);
+            Console.ReadLine();
+
+            int num3 = 8;
+            int num4 = 2;
+            int total2 = num1 - num2;
+            Console.WriteLine(num3 + " minus " + num4 + " equals " + total);
+            Console.ReadLine();
+        }
+    }
+}
