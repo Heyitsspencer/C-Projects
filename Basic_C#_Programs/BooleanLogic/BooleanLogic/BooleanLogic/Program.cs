888cf428c2df66551c2736ea2931dc0632113928
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-12-08 13:32:49 -0600

    Create BooleanLogicAssignment

---
+++ Basic_C#_Programs/BooleanLogic/BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(true && false);//AND
+            Console.WriteLine(true || true);//OR
+            Console.WriteLine(true == true);//EQUAL
+            Console.WriteLine(true != true);//NOT EQUAL
+            Console.WriteLine(true ^ true);//Will evaluate to true if one but not both are true
+            `
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/BooleanLogicAssignment/BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 1
+            Console.WriteLine("What is your age? ");
+            string age = Console.ReadLine();
+            int ageInt = Convert.ToInt32(age); //Converts age to integer
+            Console.WriteLine("Have you ever had a DUI? True/False ");
+            string dui = Console.ReadLine();
+            bool duiBool = Convert.ToBoolean(dui);
+            Console.WriteLine("How many speeding tickets do you have? ");
+            string tickets = Console.ReadLine();
+            int ticketsInt = Convert.ToInt32(tickets);//Converts tickets to integer
+            //Step 2
+            bool canBuyInsurance = (ageInt >= 15 && duiBool == false && ticketsInt < 3);
+            Console.WriteLine("Can you buy Insurance? " + canBuyInsurance);
+            Console.ReadLine();
+            
+        }
+    }
+}
