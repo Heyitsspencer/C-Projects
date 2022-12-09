2f36f396907e9afa0e12089ffc7c8dd139fc9d18
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-12-09 13:31:44 -0600

    Create Loops Project

---
+++ Basic_C#_Programs/Loops/StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Spencer";
+            //string quote = "The man said, \" Hello\", Spencer.";
+
+            //Console.WriteLine(quote);
+            //Console.ReadLine();
+
+            //string name = "Spencer";
+            //name = "joe";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Spencer");
+
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}
