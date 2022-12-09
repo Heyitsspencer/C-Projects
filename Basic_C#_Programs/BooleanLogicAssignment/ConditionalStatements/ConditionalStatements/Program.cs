5cc1c27acc6c0ad25ef1803bbfc4291a6bd7a999
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-12-09 10:24:22 -0600

    Create ConditionalStatements challenge project

---
+++ Basic_C#_Programs/BooleanLogicAssignment/ConditionalStatements/ConditionalStatements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Spencer";
+            if (name == "Steven")
+            {
+                Console.WriteLine("Your name is Steven");
+            }
+            else if (name == "Shannon")
+            {
+                Console.WriteLine("Your name is not Steven");
+            }
+            else if (name == "Aaron")
+                Console.WriteLine("Your name is Aaron");
+        }
+    }
+}
