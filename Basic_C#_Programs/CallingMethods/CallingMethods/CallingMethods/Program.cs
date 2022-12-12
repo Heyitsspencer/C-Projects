0f7dff7e704a95ad5a3e74ae96f7fc406b6998e0
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-12-12 15:34:57 -0600

    Create CallingMethods Assignment and TwentyOne Project

---
+++ Basic_C#_Programs/CallingMethods/CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 2- In the Main() program, ask the user what number they want to do the math operations on.
+            Console.WriteLine("What number do you want to do math operations on?");
+            int input = Convert.ToInt32(Console.ReadLine());
+            UserInput userInput = new UserInput();
+            
+            //Step 3- Call each method in turn, passing the user input to the method.
+            int add100 = userInput.Add100(input);
+            int squared = userInput.Squared(input);
+            int subtract10 = userInput.Subtract10(input);
+
+            //Display the returned integer to the screen.
+            Console.WriteLine(input + " plus 100 equals " + add100);
+            Console.WriteLine(input + " squared equals " + squared);
+            Console.WriteLine(input + " minus 10 equals " + subtract10);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/CallingMethods/CallingMethods/CallingMethods/UserInput.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Step 1- Create a class
+namespace CallingMethods
+{
+    public class UserInput
+    {
+        //Step 1- Create three methods, each of which that take one integer parameter in and return one integer
+        //These should do some math application on the received parameter.
+        public int Squared(int p1)
+        {
+            return p1 * p1;
+        }
+
+        public int Add100(int p1)
+        {
+            return 100 + p1;
+        }
+
+        public int Subtract10(int p1)
+        {
+            return p1 - 10;
+        }
+
+    }
+}
