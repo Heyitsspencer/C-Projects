94c748e15555d0d9203f04231343da92f1fc4d06
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-12-13 15:42:35 -0600

    Create MethodSubmission Assignment

---
+++ Basic_C#_Programs/MethodSubmission/MethodSubmission/MethodSubmission/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 2- In the Main() method of the console app, instantiate the class.
+            UserInput userInput = new UserInput();
+            //Step 3- Ask the user to input two numbers, one at a time.
+            Console.WriteLine("Enter a number: ");
+            string numberInput1 = Console.ReadLine();
+            //Step 3- Let them know they need not enter anything for the second number.
+            Console.WriteLine("Enter another number (if you want): ");
+            string numberInput2 = Console.ReadLine();
+
+            //Step 4-Call the method in the class, passing in the one or two numbers entered
+            if (numberInput2 != "")
+            {
+                int doMathOperation = userInput.DoMathOperation(Convert.ToInt32(numberInput1), Convert.ToInt32(numberInput2));
+                Console.WriteLine(numberInput1 + " + " + numberInput2 + " = " + doMathOperation);
+            }
+            else
+            {
+                int doMathOperation = userInput.DoMathOperation(Convert.ToInt32(numberInput1));
+                Console.WriteLine(numberInput1 + " + 0  = " + doMathOperation);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodSubmission/MethodSubmission/MethodSubmission/UserInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace MethodSubmission
+{
+    //Step 1- Create a class.
+    class UserInput
+    {
+        //Step 1- Create a method that takes two integers as parameters.
+        //Make one of them optional.
+        //Have the method do a math operation and return an integer result.
+        public int DoMathOperation(int p1, int p2 = 0)
+        {
+            return p1 + p2;
+        }
+    }
+}
