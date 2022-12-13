8249dc7f8eec4b6ef96443473b535de26c3436e0
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-12-13 16:31:17 -0600

    Create MethodClassAssignment

---
+++ Basic_C#_Programs/MethodClassAssignment/MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 2- In the Main() method of the console app, instantiate the class.
+            UserInput userInput = new UserInput();
+            //Step 3- Call the method in the class, passing in two numbers.
+            userInput.DoMathOperation(250, 338);
+            //Step 4- Call the method in the class, specifying the parameters by name.
+            userInput.DoMathOperation(p1: 250, p2: 338);
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodClassAssignment/MethodClassAssignment/MethodClassAssignment/UserInput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    //Step 1- Create a class
+    class UserInput
+    {
+        //Step 1- create a void method that takes two integers as parameters
+        public void DoMathOperation(int p1, int p2)
+        {
+            //Have the method do a math operation on the first integer
+            //and display the second integer to the screen.
+            int value = p1 + 100;
+            Console.WriteLine(value);
+            Console.WriteLine(p2);
+        }
+
+    }
+}
