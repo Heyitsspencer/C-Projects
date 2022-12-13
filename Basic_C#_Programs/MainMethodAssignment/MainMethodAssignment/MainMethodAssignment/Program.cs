d64ee25f66b7c0fbfcb9862f24accad5a0a82e15
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-12-13 14:04:44 -0600

    Create MainMethodAssignment

---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 2- Instantiate the class and call the one method, passing in an integer
+            UserInput userInput = new UserInput();
+            int doMathOperation = userInput.DoMathOperation(55);
+            //Step 2- Display the result to the screen
+            Console.WriteLine("55 + 100 = " + doMathOperation);
+            Console.ReadLine();
+
+            //Step 4- instantiate the class and call the second method, passing in a decimal
+            //Display the result to the screen
+            UserInput userInput2 = new UserInput();
+            int doMathOperation2 = userInput.DoMathOperation(25.7);
+            Console.WriteLine("25.7 / 2.6 = " + doMathOperation2 + " (Rounded)" );
+            Console.ReadLine();
+
+            //In the Main() method of the console app, instantiate the class and call the third method,
+            //passing in a string that equates to an integer. Display the result to the screen.
+
+            UserInput userInput3 = new UserInput();
+            int doMathOperations3 = userInput.DoMathOperation("17");
+            Console.WriteLine("17 * 200 = " + doMathOperations3);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/MainMethodAssignment/UserInput.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    //Step 1- Create a class
+    class UserInput
+    {
+        //Step 1- In that class, create a method that will take in an integer,
+        //Create a math operation for this integer, then return the answer as an integer.
+        public int DoMathOperation(int p1)
+        {
+            return p1 + 100;
+        }
+        //Step 3- add a second method to the class with the same name that will take in a decimal,
+        //create a different math operation for it and then return the answer as an 
+        public int DoMathOperation(double p1)
+        {
+            return (int)(p1 / 2.6);
+        }
+        //Step 5- Add a third method to the class, with the same name, that will take in a string, convert it to an
+        //integer if possible, do a different math operation to it, then return the answer as an integer.
+        public int DoMathOperation (string p1)
+        {
+            if (int.TryParse(p1, out int value))
+            {
+                return value * 200;
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid Input");
+            }
+        }
+    }
+}
