48c995ead56d72649fa002692a0f00a66105f795
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-12-17 10:35:22 -0600

    Create MethodsAndObjects assignment

---
+++ Basic_C#_Programs/MethodsAndObjects/MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 4- Inside of the Main method, instantiate and initialize an
+            //Employee object with a first name of “Sample” and a last name of “Student”.
+            Employee employee = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+            //Call the superclass method SayName() on the Employee object.
+            employee.SayName();
+
+        }
+    }
+}
