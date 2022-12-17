261297a780f10658984d5e8303796d205243ccbf
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-12-17 15:16:34 -0600

    Create Polymorphism assignment

---
+++ Basic_C#_Programs/PolyMorphism/Polymorphism/Polymorphism/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    //Step 2- Have your Employee class from the previous drill inherit
+    //that interface and implement the Quit() method in any way you choose.
+
+    public class Employee : IQuittable
+    {
+        public int Id { get; set; }
+
+        public void Quit()
+        {
+            Console.WriteLine("Program quitting");
+            Console.ReadLine();
+        }
+    }
+}
