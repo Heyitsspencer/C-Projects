e3c2b5fa260c5afabc1c491b644ccc1c890c1779
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-12-23 13:23:54 -0600

    Create OperatorsSubmission Assingment

---
+++ Basic_C#_Programs/OperatorsSubmision/OperatorsSubmission/OperatorsSubmission/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    class Employee
+    {
+        //Step 1- Create an Employee class with Id, FirstName and LastName properties. 
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //Step 2- In the Employee class, overload the “==” operator so it checks if two
+        //Employee objects are equal by comparing their Id property. Remember that
+        //comparison operators must be overloaded in pairs.
+        public override bool Equals(object obj)
+        {
+            Employee employee2 = (Employee)obj;
+            if (Id == employee2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            return employee1.Equals(employee2);
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return !employee1.Equals(employee2);
+        }
+    }
+}
---
+++ Basic_C#_Programs/OperatorsSubmision/OperatorsSubmission/OperatorsSubmission/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 3- In the "Program.cs" file, instantiate two objects of the Employee
+            //class and assign values to their properties. Then compare the two Employee
+            //objects using the newly overloaded operators and display the results.
+            Employee employee = new Employee
+            {
+                Id = 1,
+                FirstName = "Spencer",
+                LastName = "Davis"
+            };
+
+            Employee employee1 = new Employee
+            {
+                Id = 2,
+                FirstName = "Steven",
+                LastName = "Tolbert"
+            };
+
+            if (employee == employee1)
+            {
+                Console.WriteLine("Employee is equal to Employee1");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Employee is not equal to Employee1");
+                Console.ReadLine();
+            }
+
+            // Change ID to test
+            employee1.Id = 1;
+            if (employee == employee1)
+            {
+                Console.WriteLine("Employee is equal to Employee1");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Employee is not equal to Employee1");
+                Console.ReadLine();
+            }
+        }
+    }
+}
