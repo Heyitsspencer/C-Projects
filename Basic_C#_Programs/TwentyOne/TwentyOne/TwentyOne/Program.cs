833f12a9be73eef724fdd594a91c9e82c0229c34
Heyitsspencer <heyitsspencerdavis@gmail.com>
2023-01-08 10:08:53 -0600

    Create TwentyOne Assignment

--- Basic_C#_Programs/TwentyOne/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/TwentyOne/Program.cs
@@ -2,6 +2,9 @@
 using Casino;
 using Casino.TwentyOne;
 using System.IO;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.Generic;
 
 namespace TwentyOne
 {
@@ -16,10 +19,36 @@ namespace TwentyOne
             Console.WriteLine("Welcome to the {0}. Let's start by telling me your name.", casinoName);
             //Saves users name into a variable
             string playerName = Console.ReadLine();
-            //Asks user how much money they have
-            Console.WriteLine("How much money did you bring today?");
-            //Saves how much money the user has into a variable
-            int bank = Convert.ToInt32(Console.ReadLine());
+
+            if(playerName.ToLower() == "admin")
+            {
+                List<ExceptionEntity> Exceptions = ReadExceptions();
+                foreach (var exception in Exceptions)
+                {
+                    Console.Write(exception.Id + " | ");
+                    Console.Write(exception.ExceptionType + " | ");
+                    Console.Write(exception.ExceptionMessage + " | ");
+                    Console.Write(exception.TimeStamp + " | ");
+                    Console.WriteLine();
+                }
+                Console.Read();
+                return;
+            }
+
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                //Asks user how much money they have
+                Console.WriteLine("And how much money did you bring today?");
+                //Attempts to parse user input into integer
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                //Tells the user to only use digits if they get an error
+                if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals.");
+            }
+
+
+
             //Asks user if they want to play a game of 21
             Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
             //Saves users input into a variable and transforms it into lowercase
@@ -30,6 +59,7 @@ namespace TwentyOne
             {
                 Player player = new Player(playerName, bank);
                 player.Id = Guid.NewGuid();
+                //Writes the player Id to the log
                 using (StreamWriter file = new StreamWriter(@"C:\Users\mmcmi\OneDrive\Documents\School\C-Sharp-Projects\Basic_C#_Programs\Logs\log.txt", true))
                 {
                     file.WriteLine(player.Id);                }
@@ -40,7 +70,24 @@ namespace TwentyOne
                 //greater than 0, to play the game. If not, remove the player from the game.
                 while (player.isActivelyPlaying && player.Balance > 0)
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        UpdateDbWithException(ex);
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("An error occurred. Please contact your system administrator");
+                        UpdateDbWithException(ex);
+                        Console.ReadLine();
+                        return;
+                    }
                 }
                 game -= player;
                 Console.WriteLine("Thank you for playing!");
@@ -48,6 +95,64 @@ namespace TwentyOne
 
             Console.WriteLine("Feel free to look around the casino. Bye for now.");
             Console.ReadLine();
+        }
+        private static void UpdateDbWithException(Exception ex)
+        {
+            string connectionString = @"Data Source = (localdb)\ProjectsV13; Initial Catalog = TwentyOneGame;
+                                        Integrated Security = True; Connect Timeout = 30; Encrypt = False; 
+                                        TrustServerCertificate = False; ApplicationIntent = ReadWrite; 
+                                        MultiSubnetFailover = False";
+
+            string queryString = "INSERT INTO Exceptions (ExceptionType, ExceptionMessage, TimeStamp) VALUES" +
+                                  "(@ExceptionType, @ExceptionMessage, @TimeStamp)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@ExceptionType", SqlDbType.VarChar);
+                command.Parameters.Add("@ExceptionMessage", SqlDbType.VarChar);
+                command.Parameters.Add("@TimeStamp", SqlDbType.DateTime);
+
+                command.Parameters["@ExceptionType"].Value = ex.GetType().ToString();
+                command.Parameters["@ExceptionMessage"].Value = ex.Message;
+                command.Parameters["@TimeStamp"].Value = DateTime.Now;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+        private static List<ExceptionEntity> ReadExceptions()
+        {
+            string connectionString = @"Data Source = (localdb)\ProjectsV13; Initial Catalog = TwentyOneGame;
+                                        Integrated Security = True; Connect Timeout = 30; Encrypt = False; 
+                                        TrustServerCertificate = False; ApplicationIntent = ReadWrite; 
+                                        MultiSubnetFailover = False";
+
+            string queryString = @"Select Id, ExceptionType, ExceptionMessage, TimeStamp From Exceptions";
+
+            List<ExceptionEntity> Exceptions = new List<ExceptionEntity>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ExceptionEntity exception = new ExceptionEntity();
+                    exception.Id = Convert.ToInt32(reader["Id"]);
+                    exception.ExceptionType = reader["ExceptionType"].ToString();
+                    exception.ExceptionMessage = reader["ExceptionMessage"].ToString();
+                    exception.TimeStamp = Convert.ToDateTime(reader["TimeStamp"]);
+                    Exceptions.Add(exception);
+                }
+                connection.Close();
+            }
+            return Exceptions;
         }
     }
  }
